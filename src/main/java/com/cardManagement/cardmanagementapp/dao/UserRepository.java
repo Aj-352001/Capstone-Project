ebdeb8f8bfca8d2cf0a8cd02b1dca130d9c39776
Aj-352001 <77727866+Aj-352001@users.noreply.github.com>
2023-09-05 17:49:57 +0530

    Delete src/main/java/com/cardManagement/cardmanagementapp/dao/UserRepository.java

--- src/main/java/com/cardManagement/cardmanagementapp/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cardManagement.cardmanagementapp.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cardManagement.cardmanagementapp.entities.AppUser;
-
-public interface UserRepository extends JpaRepository<AppUser, Integer>{
-
-}
