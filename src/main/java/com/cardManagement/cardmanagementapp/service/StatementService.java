bdf19e7fae3247ea1c4464a674c25e8d2e2bb15e
Aj-352001 <77727866+Aj-352001@users.noreply.github.com>
2023-09-05 17:39:46 +0530

    Create StatementService.java

---
+++ src/main/java/com/cardManagement/cardmanagementapp/service/StatementService.java
@@ -0,0 +1,19 @@
+package com.cardManagement.cardmanagementapp.service;
+import java.util.List;
+
+import com.cardManagement.cardmanagementapp.entities.Statement;
+import com.cardManagement.cardmanagementapp.exceptions.StatementException;
+
+public interface StatementService {
+	
+	Statement getStatementbystatementId (Integer statementId) throws StatementException ;
+	
+	Statement addStatement (Statement newStatement) throws StatementException ; 
+	
+	Statement updateStatement (Statement newStatement) throws StatementException ; 
+	
+	Statement interestCalculation (Integer statementId,Double amount) throws StatementException;
+
+	List <Statement> getAllStatements();
+
+}
