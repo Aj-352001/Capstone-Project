1a9d4100a45c00befdc9568cf346768265b2de3a
Aj-352001 <77727866+Aj-352001@users.noreply.github.com>
2023-09-05 17:51:18 +0530

    Update ApplicationService.java

--- src/main/java/com/cardManagement/cardmanagementapp/service/ApplicationService.java
+++ src/main/java/com/cardManagement/cardmanagementapp/service/ApplicationService.java
@@ -1,16 +1,22 @@
 package com.cardManagement.cardmanagementapp.service;
 
 import java.util.Collection;
+import java.util.List;
 
+import com.cardManagement.cardmanagementapp.dto.ApplicationDto;
 import com.cardManagement.cardmanagementapp.entities.Application;
 import com.cardManagement.cardmanagementapp.entities.CardApprovalStatus;
 import com.cardManagement.cardmanagementapp.exceptions.ApplicationException;
 
 public interface ApplicationService {
-	
-	String createApplication(Application newApplication) throws ApplicationException;
-	Application displayApplicationById(Integer id) throws ApplicationException;
-	Collection<Application> displayAllApplications();
-	Application updateApplicationStatus(Integer id, CardApprovalStatus status) throws ApplicationException;
+
+	String createApplication(ApplicationDto newApplication) throws ApplicationException;
+
+	ApplicationDto displayApplicationById(Integer id) throws ApplicationException;
+
+	List<ApplicationDto> displayAllApplications();
+
+	ApplicationDto updateApplicationStatus(Integer id, CardApprovalStatus status) throws ApplicationException;
+
 	String deleteApplicationById(Integer id) throws ApplicationException;
 }
